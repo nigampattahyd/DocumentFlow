5db6cf56cdc1e791a5e6bd84ffbb616b600eb970
nikhil4228 <nikhil4228@gmail.com>
2016-09-14 21:35:12 -0500

    Adding the Content LAyout Page

--- DocumentFlow/Controllers/TestController.cs
+++ DocumentFlow/Controllers/TestController.cs
@@ -13,5 +13,9 @@ namespace DocumentFlow.Controllers
         {
             return View();
         }
+        public ActionResult Sample()
+        {
+            return View();
+        }
     }
 }
