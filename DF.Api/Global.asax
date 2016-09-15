18dcbda308f92ccdbe186d95156f64d9cbdd64e8
nikhil4228 <nikhil4228@gmail.com>
2016-09-14 22:13:55 -0500

    Adding New Web API

---
+++ DF.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DF.Api.WebApiApplication" Language="C#" %>
---
+++ DocumentFlow/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace DocumentFlow.App_Start
+{
+    public class BundleConfig
+    {
+        //public static void RegisterBundles(BundleCollection )
+        //{
+
+        //}
+    }
+}
