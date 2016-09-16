0a3df0d959c500aa0219cdbbed3474b0d1533b6b
nikhil4228 <nikhil4228@gmail.com>
2016-09-15 22:49:15 -0500

    Added all the scripts and styles to bundling and minification

--- DocumentFlow/App_Start/BundleConfig.cs
+++ DocumentFlow/App_Start/BundleConfig.cs
@@ -10,17 +10,53 @@ namespace DocumentFlow.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            ScriptBundle Scripts = new ScriptBundle("~/Scripts/");
-            Scripts.Include("~/Scripts/jquery-{version}.js",
-                                "~/Scripts/bootstrap.min.js");
-            Scripts.IncludeDirectory("~/Content/plugins/", "*.js", true);
-            Scripts.IncludeDirectory("~/Content/dist/", "*.js", true);
-            bundles.Add(Scripts);
-            BundleTable.EnableOptimizations = true;
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                             "~/Scripts/jquery-{version}.js"));
 
-            StyleBundle Styles = new StyleBundle("~/Content/");
-            Styles.IncludeDirectory("~/Content/css/", "*.css", true);
-            Styles.IncludeDirectory("~/Content/plugins/", "*.css", true);
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/customJs").Include(
+                "~/Content/js/bootstrap.min.js",
+                "~/Content/plugins/jQuery/jquery-2.2.3.min.js",
+"~/Content/plugins/fastclick/fastclick.js",
+"~/Content/dist/js/app.min.js",
+"~/Content/plugins/sparkline/jquery.sparkline.min.js",
+"~/Content/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js",
+"~/Content/plugins/jvectormap/jquery-jvectormap-world-mill-en.js",
+"~/Content/plugins/slimScroll/jquery.slimscroll.min.js",
+"~/Content/plugins/chartjs/Chart.min.js",
+"~/Content/dist/js/pages/dashboard2.js",
+"~/Content/dist/js/demo.js",
+"~/Scripts/jquery-1.10.2.min.js",
+"~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/css/bootstrap.css",
+                      "~/Content/site.css",
+                      "~/Content/css/bootstrap-slider/slider.css"));
+            bundles.Add(new StyleBundle("~/Content/css/Customcss").Include(
+                    "~/Content/css/font-awesome/ionicons.min.css",
+    "~/Content/css/bootstrap.min.css",
+    "~/Content/css/font-awesome/font-awesome.min.css",
+    "~/Content/dist/css/AdminLTE.css",
+    "~/Content/dist/css/skins/_all-skins.min.css",
+    "~/Content/plugins/iCheck/flat/blue.css",
+    "~/Content/plugins/morris/morris.css",
+    "~/Content/plugins/jvectormap/jquery-jvectormap-1.2.2.css",
+    "~/Content/plugins/datepicker/datepicker3.css",
+    "~/Content/plugins/daterangepicker/daterangepicker.css",
+    "~/Content/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css" ));
+
+
         }
     }
 }
