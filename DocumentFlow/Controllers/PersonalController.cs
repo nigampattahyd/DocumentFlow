3cb3c25b0d896943dad319b7d21fbbc8c56b1eb4
nikhil4228 <nikhil4228@gmail.com>
2016-11-25 01:58:55 -0600

    adding test file

--- DocumentFlow/Controllers/PersonalController.cs
+++ DocumentFlow/Controllers/PersonalController.cs
@@ -74,7 +74,7 @@ namespace DocumentFlow.Controllers
         [HttpGet]
         public ActionResult SAmpleDesign()
         {
-            User loggedinUser = _userRepository.getUserDetailsById(10031);
+            User loggedinUser = _userRepository.getUserDetailsById(10034);
             return View(loggedinUser);
         }
         [HttpPost]
